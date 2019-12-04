22ecd4f01d6ccd2c3b037e3b5c0bfe84e442959d
Tom Hennessy <tmhennessy@outlook.com>
2019-12-04 13:28:14 +0000

    Tic-tac-toe
    I have loaded the tutorial given on the react site so that i can use it to learn.
    
    I have played with it and am ready to begin making my UX

--- js/default.js
+++ js/default.js
@@ -2,24 +2,15 @@ $api_url = 'http://localhost:8080';
 
 var opts = {'message': 'test'};
 
-// fetch($api_url, {
-// 	method: 'post',
-// 	// dataType: 'json',
-// 	body: JSON.stringify(opts)
-// }).then(function(response) {
-// 	console.log( response.json() );
-// }).then(function(data) {
-// 	console.log('message', data.message);
+// fetch($api_url).then(function(res) {
+// 	// res instanceof Response == true.
+// 	if (res.ok) {
+// 		res.json().then(function(data) {
+// 			console.log(data.entries);
+// 		});
+// 	} else {
+// 		console.log("Looks like the response wasn't perfect, got status", res.status);
+// 	}
+// }, function(e) {
+// 	console.log("Fetch failed!", e);
 // });
-fetch($api_url).then(function(res) {
-  // res instanceof Response == true.
-  if (res.ok) {
-    res.json().then(function(data) {
-      console.log(data.entries);
-    });
-  } else {
-    console.log("Looks like the response wasn't perfect, got status", res.status);
-  }
-}, function(e) {
-  console.log("Fetch failed!", e);
-});
