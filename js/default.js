f2fa53615de585c423c8d54890dd151927f4804e
Tom Hennessy <tmhennessy@outlook.com>
2019-12-05 14:15:40 +0000

    Added jquery back
    Added jquery again to debug the CORS issue

--- js/default.js
+++ js/default.js
@@ -1,9 +1,20 @@
 $api_url = 'http://localhost:8080';
 
 var opts = {'message': 'test'};
-
+$.ajax({
+	type 		: 'GET',
+	url 		: $api_url,
+	crossDomain : true,
+	success : function(data){
+		console.log('success');
+	},
+	error : function(e){
+		console.log('error: ',e);
+	}
+});
 // fetch($api_url).then(function(res) {
 // 	// res instanceof Response == true.
+// 	console.log('outside');
 // 	if (res.ok) {
 // 		res.json().then(function(data) {
 // 			console.log(data.entries);
